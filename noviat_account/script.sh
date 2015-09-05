78c07fac4047e15aac7478db7cf2111d341bca9a
dro <dro@elneo.com>
2015-09-05 10:15:49 +0200

    [ADD] Ajout de account_move_line_balance

--- noviat_account/script.sh
+++ noviat_account/script.sh
@@ -42,7 +42,7 @@ fi
 goon=
 while [ -z $goon ]
 do
-    echo -n 'This will update noviat_account modules. Do you want to continue? '
+    echo -n 'This will update noviat_account modules. Do you want to continue (y/n)? '
     read goon
     if [ $goon = 'n' ]
     then
@@ -163,3 +163,68 @@ python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_op
 python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_overdue
 
 python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_asset_management_xls
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_invoice_line_import
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_invoice_pay_filter
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_invoice_sequence_by_period
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_invoice_tax_manual
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_journal_period_close
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_move_import
+
+psql -d $DATABASE -X --echo-all -v ON_ERROR_STOP=1 -f ./7_account_line_balance.sql
+
+
+if [ $? != 0 ]; then
+    echo "psql failed while trying to run this sql script" 1>&2
+    exit 1
+fi
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_move_line_balance
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_reversal
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_journal_period_close
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_tax_constraints
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_trial_balance_period_xls
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL base_vat_enhancements
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL datetime_widget
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_journal_period_close
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL intrastat_base
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL intrastat_product
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL account_journal_period_close
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL invoice_fiscal_position_update
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL l10n_be_coda_batch
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL multi_company_fix
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL sale_order_attachments
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL tree_date_search_extension
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL web_dialog_size
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL web_export_view
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL web_fix_binaryfile
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL web_sheet_full_width
+
+python ./module_install.py -d $DATABASE -u $USER -w $PASSWORD -s $URL web_sheet_full_width_selective
+
+
+
