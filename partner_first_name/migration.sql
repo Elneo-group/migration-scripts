769662ec33f6536d45b4223c908279f3b7da0329
elneo-cth <clement.thomas@elneo.com>
2015-10-07 09:54:39 +0200

    ajout de partner_firstname

---
+++ partner_first_name/migration.sql
@@ -0,0 +1,2 @@
+update res_partner set lastname = a.last_name, firstname = a.first_name from res_partner_address a where a.old_partner_id = res_partner.id;
+update res_partner set firstname = split_part(u.name,' ',1), lastname = split_part(u.name,' ',2) from res_users u where u.partner_id = res_partner.id;
