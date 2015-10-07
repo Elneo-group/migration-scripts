26f0f9525b0cdaa1efd99b864807d34cf3f4db16
elneo-cth <clement.thomas@elneo.com>
2015-10-07 14:46:17 +0200

    account script

--- account/migration.sql
+++ account/migration.sql
@@ -1,2 +1,2 @@
 --change conditions de paiement par défault à "comptant"
-update ir_property set value_reference = 'account.payment.term,2' where name = 'property_payment_term' and res_id is null;
+update ir_property set value_reference = 'account.payment.term,8' where name = 'property_payment_term' and res_id is null;
