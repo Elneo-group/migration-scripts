109b54b8469f9b1af6071a73305dab0e972f9e5a
elneo-cth <clement.thomas@elneo.com>
2015-10-09 13:59:41 +0200

    correction partner_id des ventes dans le script crm

--- crm/migration.sql
+++ crm/migration.sql
@@ -1,2 +1,5 @@
 --delete partner_categories
 truncate table res_partner_category cascade;
+update sale_order set partner_id = p.parent_id from res_partner p 
+where sale_order.partner_id = p.id
+and (not p.is_company or p.is_company is null) and p.parent_id is not null;
