5628899ed35a44cda18f5072560b06928ff999f9
dro <dro@elneo.com>
2015-11-19 21:56:43 +0100

    [FIX] Modifications

--- intrastat/after.sql
+++ intrastat/after.sql
@@ -47,11 +47,11 @@ JOIN ir_model_fields imf ON imf.model = 'product.template' AND imf.name = 'hs_co
 WHERE NOT EXISTS (SELECT id FROM ir_property WHERE res_id = 'product.template,' || pt.id AND name = 'hs_code_id');
 
 -- DEFAULT VALUES
-UPDATE res_company SET intrastat = 'extended';
-UPDATE res_company SET intrastat_arrivals = 'extended';
-UPDATE res_company SET intrastat_dispatches = 'extended';
+UPDATE res_company SET intrastat = 'standard';
+UPDATE res_company SET intrastat_arrivals = 'standard';
+UPDATE res_company SET intrastat_dispatches = 'standard';
 
-UPDATE account_invoice SET intrastat = 'extended' WHERE company_id = 1;
+UPDATE account_invoice SET intrastat = 'standard' WHERE company_id = 1;
 
 
 -- GET HS CODE FOR INVOICES
@@ -85,7 +85,7 @@ $$
 	
 
 	END;
-$$ LANGUAGE plpgsql
+$$ LANGUAGE plpgsql;
 
 
 -- INVOICES
@@ -94,7 +94,7 @@ FROM
 	(SELECT req1.*, hc.id AS hc_id FROM
 		(SELECT ail.id AS id, GetProdIntrastat(pt.id) AS intra_id, pt.id AS pt_id
 		FROM account_invoice_line AS ail
-			JOIN account_invoice ai ON ail.invoice_id = ai.id AND ai.state = 'draft'
+			JOIN account_invoice ai ON ail.invoice_id = ai.id AND to_char(ai.date_invoice,'YYYY') = '2015'
 			JOIN product_product pp ON pp.id = ail.product_id
 			JOIN product_template pt ON pt.id = pp.product_tmpl_id AND pt.type <> 'service'
 		WHERE ail.product_id IS NOT NULL) req1
@@ -102,4 +102,62 @@ FROM
 	JOIN hs_code hc ON hc.local_code = ric.intrastat_code
 	) req2
 	
-WHERE account_invoice_line.id = req2.id
+WHERE account_invoice_line.id = req2.id;
+
+
+-- IMPORT FORMER INTRASTAT DECLARATIONS
+
+INSERT INTO l10n_be_intrastat_product_declaration(id, company_id,num_decl_lines,total_amount,state,type,revision,reporting_level,note,company_country_code,year,month,year_month,action,create_date,create_uid,write_date,write_uid)
+
+SELECT rib.id,
+rib.company_id,
+rib.num_lines AS num_decl_lines,
+rib.total_amount,
+CASE WHEN rib.state = 'edit' THEN 'draft' ELSE rib.state END AS state,
+CASE WHEN rib.ttype = 'A' THEN 'arrivals' WHEN rib.ttype = 'D' THEN 'dispatches' ELSE NULL END AS type,
+rib.revision,
+CASE WHEN rib.extended = True THEN 'extended' WHEN rib.extended = False THEN 'standard' ELSE NULL END AS reporting_level,
+rib.notes AS note,
+lower(rco.code) AS company_country_code,
+CAST(to_char(rib.end_date,'YYYY') AS integer) AS year,
+CAST(to_char(rib.end_date,'MM') AS integer) AS month,
+to_char(rib.end_date,'YYYY') || '-' || to_char(rib.end_date,'MM') AS year_month,
+'replace' AS action,
+
+rib.create_date AS create_date,
+rib.create_uid AS create_uid,
+rib.write_date AS write_date,
+rib.write_uid AS write_uid
+
+FROM report_intrastat_belgium rib
+JOIN res_company rc ON rc.id = rib.company_id
+JOIN res_partner rp ON rc.partner_id = rp.id
+JOIN res_country rco ON rco.id = rp.country_id ;
+
+-- IMPORT FORMER INTRASTAT DECLARATION LINES
+
+INSERT INTO l10n_be_intrastat_product_declaration_line(id,parent_id,transport_id,region_id,weight,amount_company_currency,transaction_id,incoterm_id,hs_code_id,src_dest_country_id,suppl_unit_qty,create_date,create_uid,write_date,write_uid)
+
+SELECT ribl.id,
+ribl.parent_id AS parent_id,
+ribl.transport AS transport_id,
+ribl.gewest AS region_id,
+CAST(ribl.weight AS integer) AS weight,
+ribl.amount_company_currency,
+ribl.transaction AS transaction_id,
+ribl.incoterm_id,
+hc.id AS hs_code_id,
+ribl.country_id AS src_dest_country_id,
+CAST(ribl.quantity AS integer) AS suppl_unit_qty,
+
+
+ribl.create_date AS create_date,
+ribl.create_uid AS create_uid,
+ribl.write_date AS write_date,
+ribl.write_uid AS write_uid
+
+
+FROM report_intrastat_belgium_line ribl
+JOIN hs_code hc ON hc.local_code = ribl.intrastat_code
+RIGHT OUTER JOIN report_intrastat_belgium rib ON rib.id = ribl.parent_id
+WHERE ribl.id IS NOT NULL;
--- intrastat/before.sql
+++ intrastat/before.sql
@@ -1,3 +1,21 @@
 UPDATE ir_module_module SET state = 'uninstalled' WHERE name = 'l10n_be_intrastat';
+UPDATE ir_module_module SET state = 'uninstalled' WHERE name = 'report_intrastat';
 
-UPDATE ir_ui_view SET active = False WHERE xml_id LIKE 'l10n_be_intrastat.%';
+DELETE FROM ir_ui_view WHERE id = 2628;
+DELETE FROM ir_ui_view WHERE id = 2627;
+DELETE FROM ir_ui_view WHERE id = 2632;
+DELETE FROM ir_ui_view WHERE id = 2631;
+DELETE FROM ir_ui_view WHERE id = 2630;
+DELETE FROM ir_ui_view WHERE id = 2629;
+
+DELETE FROM ir_ui_view WHERE id = 2633;
+DELETE FROM ir_ui_view WHERE id = 2638;
+DELETE FROM ir_ui_view WHERE id = 2639;
+DELETE FROM ir_ui_view WHERE id = 2641;
+DELETE FROM ir_ui_view WHERE id = 2640;
+
+DELETE FROM ir_ui_view WHERE id = 2637;
+DELETE FROM ir_ui_view WHERE id = 2634;
+DELETE FROM ir_ui_view WHERE id = 2635;
+DELETE FROM ir_ui_view WHERE id = 2636;
+DELETE FROM ir_ui_view WHERE id = 2642;
