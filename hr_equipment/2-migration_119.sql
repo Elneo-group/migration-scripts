681eb847839539384b0bc846c46696cad7d17fd6
elneo-cth <clement.thomas@elneo.com>
2015-12-01 14:54:33 +0100

    *

--- hr_equipment/2-migration_119.sql
+++ hr_equipment/2-migration_119.sql
@@ -3,10 +3,8 @@ DECLARE
 DB_BACKUP_FILE varchar;
 BEGIN
 
-truncate table hr_equipment cascade;
-truncate table hr_equipment_category cascade;
-
-alter sequence hr_equipment_category_id_seq restart with 1;
+drop table hr_equipment if exists;
+drop table hr_equipment_category if exists;
 
 
 END;
