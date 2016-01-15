34f3d3ab682752a7ef25014e733c56176a5de800
dro <dro@elneo.com>
2016-01-15 12:02:43 +0100

    [FIX] Suppression des anciennes vues l10n_be

--- noviat_account/2_update_model_data.sql
+++ noviat_account/2_update_model_data.sql
@@ -1,2 +1,3 @@
 update ir_model_data set module='l10n_be_partner' where module='l10n_be_coda' and model='res.bank';
 update ir_model_data set module='l10n_be_partner' where module='l10n_be_coa_multilang' and model='res.partner.title';
+DELETE FROM ir_model_data WHERE module = 'l10n_be';
