c2f778ec097e90ba06c56e0c7414578d14bf1322
elneo-cth <clement.thomas@elneo.com>
2016-01-18 15:44:40 +0100

    *

--- noviat_account/2_update_model_data.sql
+++ noviat_account/2_update_model_data.sql
@@ -2,3 +2,4 @@ update ir_model_data set module='l10n_be_partner' where module='l10n_be_coda' an
 update ir_model_data set module='l10n_be_partner' where module='l10n_be_coa_multilang' and model='res.partner.title';
 DELETE FROM ir_model_data WHERE module = 'l10n_be';
 delete from ir_ui_view where id in (select v.id from ir_model_data d left join ir_ui_view v on d.res_id = v.id where d.model = 'ir.ui.view' and module = 'l10n_be');
+delete from ir_ui_view where name = 'Partner VAT intra';
